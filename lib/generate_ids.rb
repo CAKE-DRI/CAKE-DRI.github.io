5b9ccc8cbbabd040ac2eef685be43cade1be96d4
Nick Brown <nick@mesham.com>
2025-09-15 20:38:44 +0100

    Update generate_ids.rb

--- lib/generate_ids.rb
+++ lib/generate_ids.rb
@@ -1,19 +1,10 @@
 require 'securerandom'
 
-Dir.glob("_posts/*.md") do |file|
+Dir.glob("_collections/**/*.md") do |file|
   content = File.read(file)
   unless content =~ /unique_id:/
-    unique_id = SecureRandom.hex(4) # Generates a 8-character hex ID (e.g., "a1b2c3d4")
-    new_content = content.sub(/^---\n/, "---\nunique_id: #{unique_id}\n")
-    File.write(file, new_content)
-  end
-end
-
-Dir.glob("*.md") do |file| # For pages
-  content = File.read(file)
-  unless content =~ /unique_id:/ || File.basename(file) == "_config.yml"
-    unique_id = SecureRandom.hex(4)
-    new_content = content.sub(/^---\n/, "---\nunique_id: #{unique_id}\n")
+    unique_id = SecureRandom.random_number(10000).to_i # Generates a 8-character hex ID (e.g., "a1b2c3d4")
+    new_content = content.sub(/^---\n/, "---\nunique_id: #{random_id}\n")
     File.write(file, new_content)
   end
 end
